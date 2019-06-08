4fb7b6386303ae89fc2360cc11d2a6056a2da682
Brian Martin <bmartin5692@gmail.com>
2019-06-08 16:55:42 -0400

    Update test_z_problem.py
    fix for windows tests

--- tests/test_z_problem.py
+++ tests/test_z_problem.py
@@ -70,23 +70,23 @@ def test_createcert(mock_run, mock_platform, mock_machine, mock_exec):
     platform.system.return_value = "darwin"
     bumper.create_certs()
     assert mock_run.called == True
-    assert "../create_certs/create_certs_osx" in mock_exec.call_args.args[0]
+    assert os.path.join("..", "create_certs", "create_certs_osx") in mock_exec.call_args.args[0]
 
     platform.system.return_value = "windows"
     bumper.create_certs()
     assert mock_run.called == True
-    assert "../create_certs/create_certs_windows.exe" in mock_exec.call_args.args[0]
+    assert os.path.join("..", "create_certs", "create_certs_windows.exe") in mock_exec.call_args.args[0]
 
     platform.system.return_value = "linux"
     bumper.create_certs()
     assert mock_run.called == True
-    assert "../create_certs/create_certs_linux" in mock_exec.call_args.args[0]
+    assert os.path.join("..", "create_certs","create_certs_linux") in mock_exec.call_args.args[0]
 
     platform.system.return_value = "linux"
     platform.machine.return_value = "arm"
     bumper.create_certs()
     assert mock_run.called == True
-    assert "../create_certs/create_certs_rpi" in mock_exec.call_args.args[0]
+    assert os.path.join("..", "create_certs", "create_certs_rpi") in mock_exec.call_args.args[0]
 
     with LogCapture() as l:
         platform.system.return_value = "nixbad"
